bf38398f404d36b01c5fc7f9048beb791a46dedb
David Alonso // Solvos <david.alonso@solvos.es>
2022-12-13 09:47:00 +0100

    [ADD] mis_builder_extra_query_filter

---
+++ mis_builder_extra_query_filter/__manifest__.py
@@ -0,0 +1,14 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Extra query filters to MIS Builder",
+    "summary": """
+        Adds extra query filters to MIS Builder
+    """,
+    "author": "Solvos",
+    "license": "AGPL-3",
+    "version": "11.0.1.0.0",
+    "website": "https://github.com/solvosci/slv-mis-builder",
+    "depends": ["mis_builder_budget"],
+    "installable": True,
+}
