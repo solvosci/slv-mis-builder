87588d6c0040f1e32468bbb811bde83e8982abec
Christian Santamaria <christian.santamaria@solvos.es>
2024-05-20 17:13:15 +0200

    [ADD] project_mis_report

---
+++ project_mis_report/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Project Mis Reports",
+    "summary": """
+        Link Project with Mis Report
+    """,
+    "author": "Solvos",
+    "license": "AGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Project",
+    "website": "https://github.com/solvosci/slv-mis-builder",
+    "depends": ["project","mis_builder"],
+    "data": [
+        "views/project_project_views.xml",
+    ],
+    "installable": True,
+}
---
+++ project_mis_report/models/project_project.py
@@ -0,0 +1,67 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models, _
+from odoo.exceptions import ValidationError
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
+
+class ProjectProject(models.Model):
+    _inherit = "project.project"
+
+    mis_report_template_id = fields.Many2one('mis.report', string="Mis Report Template")
+    mis_report_instance_id = fields.Many2one('mis.report.instance', string="Mis Report")
+    is_group_account_manager = fields.Boolean(compute='_compute_is_group_account_manager')
+    is_group_project_manager = fields.Boolean(compute='_compute_is_group_project_manager')
+
+    def _compute_is_group_project_manager(self):
+        self.write({
+            "is_group_project_manager": self.user_has_groups("project.group_project_manager"),
+        })
+
+    def _compute_is_group_account_manager(self):
+        self.write({
+            "is_group_account_manager": self.user_has_groups("account.group_account_manager"),
+        })
+
+    def generate_monthly_dates(self):
+        result = []
+        current_date = self.date_start.replace(day=1)
+        end_date = self.date.replace(day=1)
+        end_date += relativedelta(months=1)
+
+        while current_date <= end_date:
+            result.append(current_date)
+            current_date += relativedelta(months=1)
+        return result
+
+    def auto_create_mis_instance(self):
+        self.ensure_one()
+        if not self.analytic_account_id:
+            raise ValidationError(
+                _('The Analytical Account field must be covered')
+            )
+        instance_id = self.mis_report_instance_id.create({
+            'name': _('Project: %s') % self.name,
+            'report_id': self.mis_report_template_id.id,
+            'comparison_mode': True,
+            'date': self.date_start,
+            'analytic_account_id': self.analytic_account_id.id
+        })
+
+        months_range = self.generate_monthly_dates()
+
+        count_month = 0
+        for month in months_range:
+            period_id = instance_id.period_ids.create({
+                'report_instance_id': instance_id.id,
+                'name': '%s/%s' % (month.strftime("%m"), month.strftime("%Y")),
+                'source': 'actuals',
+                'mode': 'relative',
+                'type': 'm',
+                'offset': count_month,
+                'duration': 1,
+            })
+            count_month += 1
+        self.mis_report_instance_id = instance_id
