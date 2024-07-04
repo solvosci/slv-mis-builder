caf1d65adc758d326643d81606d2a8ccca5ff9b8
Christian Santamaria <christian.santamaria@solvos.es>
2024-07-04 10:56:01 +0200

    [IMP] project_mis_report: add TOTAL period

--- project_mis_report/__manifest__.py
+++ project_mis_report/__manifest__.py
@@ -8,7 +8,7 @@
     """,
     "author": "Solvos",
     "license": "AGPL-3",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.1.0",
     "category": "Project",
     "website": "https://github.com/solvosci/slv-mis-builder",
     "depends": ["project","mis_builder"],
