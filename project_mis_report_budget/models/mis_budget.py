3723a17ae3fc38faeabebc3394d29051d7505944
Christian Santamaria <christian.santamaria@solvos.es>
2024-08-07 11:04:44 +0200

    [IMP] project_mis_report_budget: recalculate margin budget When change project end date recalculate margin budget

--- project_mis_report_budget/models/mis_budget.py
+++ project_mis_report_budget/models/mis_budget.py
@@ -9,3 +9,4 @@ class MisBudget(models.Model):
 
     project_id = fields.Many2one('project.project')
     analytic_account_project_id = fields.Many2one('account.analytic.account', related='project_id.analytic_account_id')
+    is_margin = fields.Boolean()
--- project_mis_report_budget/models/project_project.py
+++ project_mis_report_budget/models/project_project.py
@@ -13,33 +13,37 @@ class ProjectProject(models.Model):
 
     def auto_create_mis_instance(self):
         super().auto_create_mis_instance()
-
         margin_kpi_ids = self.mis_report_template_id.kpi_ids.filtered(lambda x: x.kpi_type == 'margin')
         expression_id = margin_kpi_ids and margin_kpi_ids.expression_ids[0].id or False
 
         if expression_id:
-            budget_id = self.env['mis.budget'].create({
-                'name': '%s - %s' % (self.name, _('MARGINS')),
-                'project_id': self.id,
-                'report_id': self.mis_report_template_id.id,
-                'date_from': self.date_start.replace(day=1),
-                'date_to': self.date.replace(day=1) + relativedelta(months=2) - relativedelta(days=1),
-            })
+            budget_id = self.mis_budget_ids.filtered(lambda x: x.is_margin)
+
+            if not budget_id:
+                budget_id = self.env['mis.budget'].create({
+                    'name': '%s - %s' % (self.name, _('MARGINS')),
+                    'project_id': self.id,
+                    'report_id': self.mis_report_template_id.id,
+                    'date_from': self.date_start.replace(day=1),
+                    'date_to': self.date.replace(day=1) + relativedelta(months=2) - relativedelta(days=1),
+                    'is_margin': True
+                })
+            else:
+                budget_id.date_to = self.date.replace(day=1) + relativedelta(months=2) - relativedelta(days=1)
 
             months = self.generate_monthly_dates()
-
             for month in months:
                 month_end = month.replace(day=1) + relativedelta(months=1) - relativedelta(days=1)
-
-                budget_id.item_ids.create({
-                    'name': _('MARGINS'),
-                    'budget_id': budget_id.id,
-                    'kpi_expression_id': expression_id,
-                    'date_from': month,
-                    'date_to': month_end,
-                    'amount': self.margin * 100,
-                    'analytic_account_id': self.analytic_account_id.id
-                })
+                if month not in budget_id.item_ids.mapped('date_from'):
+                    budget_id.item_ids.create({
+                        'name': _('MARGINS'),
+                        'budget_id': budget_id.id,
+                        'kpi_expression_id': expression_id,
+                        'date_from': month,
+                        'date_to': month_end,
+                        'amount': self.margin * 100 if not budget_id.item_ids else budget_id.item_ids[-1].amount,
+                        'analytic_account_id': self.analytic_account_id.id
+                    })
             budget_id.action_confirm()
 
     def write(self, vals):
