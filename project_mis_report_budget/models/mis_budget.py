37056e0ce5ef2c3424e2c14758945fdc0bc26c88
Christian Santamaria <christian.santamaria@solvos.es>
2024-08-08 13:28:46 +0200

    [IMP] project_mis_report_budget: is_margin field readonly

--- project_mis_report_budget/models/mis_budget.py
+++ project_mis_report_budget/models/mis_budget.py
@@ -9,7 +9,7 @@ class MisBudget(models.Model):
 
     project_id = fields.Many2one('project.project')
     analytic_account_project_id = fields.Many2one('account.analytic.account', related='project_id.analytic_account_id')
-    is_margin = fields.Boolean()
+    is_margin = fields.Boolean(readonly=True)
 
     def action_duplicate(self):
         budget_id = self.copy()
