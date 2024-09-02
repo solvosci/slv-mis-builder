17d3a1be7764759aa6876df987c6be8b97624a33
Ángel Rivas <angel.rivas@solvos.es>
2024-09-02 13:22:18 +0200

    [FIX] project_mis_report: add validation to date_start and date fields must be covered

--- project_mis_report/__manifest__.py
+++ project_mis_report/__manifest__.py
@@ -8,7 +8,7 @@
     """,
     "author": "Solvos",
     "license": "AGPL-3",
-    "version": "15.0.1.2.0",
+    "version": "15.0.1.2.1",
     "category": "Project",
     "website": "https://github.com/solvosci/slv-mis-builder",
     "depends": ["project","mis_builder"],
